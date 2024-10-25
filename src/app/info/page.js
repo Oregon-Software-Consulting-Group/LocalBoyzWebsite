1e7f360cf82a09ddbaba2a590a799ac93dfc92e1
Dhruv Desai <dhruv@10-249-178-40.wireless.oregonstate.edu>
2024-10-24 19:03:30 -0700

    info page

---
+++ src/app/info/page.js
@@ -0,0 +1,43 @@
+export default function page(){
+    return(
+        <div>
+            <h1 style={{
+                fontSize: '40px', 
+                fontFamily: 'freight-text-pro', 
+                fontWeight:'bold', 
+                padding: '20px', 
+                position: 'relative',
+                paddingLeft: '150px',
+                paddingTop: '50px'
+            }}>
+                Location
+            </h1>
+
+            <h2 style={{
+                fontSize: '20px',
+                paddingLeft: '150px',
+                position: 'relative',
+                fontFamily: 'freight-text-pro'        
+            }}> 
+                1425 NW Monroe Ave, <br />
+                Corvallis, OR 97330
+            </h2>   
+
+            <h1 style={{
+                fontSize: '40px',
+                paddingLeft: '150px',
+                position: 'relative',
+                fontFamily: 'freight-text-pro', 
+                fontWeight:'bold', 
+
+            }}>
+                Hours
+            </h1>
+
+
+        </div> 
+    );
+}
+
+
+
